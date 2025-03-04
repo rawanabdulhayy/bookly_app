dd557abbabd8874edd773e46a6640fab6aab8801
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-03-04 22:58:46 +0200

    added the first screen: splashview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,6 +12,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
